afac5e674ba1836ef1ef6ddc0d449a3f821d43d6
Qiming Teng <tengqm@outlook.com>
2026-07-18 16:35:55 +0800

    chore: add Apache 2.0 copyright header to all source files
    Copyright 2026 FlagOS Contributors
    
    This PR was written in part with the assistance of generative AI.

--- src/flagtensor/cutensor.py
+++ src/flagtensor/cutensor.py
@@ -1,3 +1,17 @@
+# Copyright 2026 FlagOS Contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import ctypes
 from dataclasses import dataclass
 from ctypes import POINTER, byref, c_double, c_float, c_int, c_int32, c_int64, c_uint32, c_uint64, c_void_p
@@ -2394,6 +2408,3 @@ def contraction(a, b, *, c=None, alpha=1.0, beta=0.0, mode_a=None, mode_b=None, 
         mode_d=mode_d,
         out=out,
     )
-
-
-
